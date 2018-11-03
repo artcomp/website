99ab23b137e369f83f656f5def3d83e6183b34cd
artcomp <arthurfreitas72@gmail.com>
2018-11-03 18:32:22 -0300

    Add flask

---
+++ application.py
@@ -0,0 +1,19 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/avaliar')
+def avaliar():
+	return render_template("avaliar.html")
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
